788cfd9763914161a7d2a3bbce627b3b2a806a92
Francisco Sánchez Mayorga <franciscosanchezmayorga@gmail.com>
2021-11-22 09:40:43 +0000

    WIP project creation errors

--- ui.frontend/src/main/webpack/components/_list.js
+++
@@ -1,9 +0,0 @@
-function collapseMenu() {
-    var x = document.getElementById("header__topnav");
-    if (x.className === "pzbf-header__topnav") {
-        x.className += "--mobile";
-        console.log("x");
-    } else {
-        x.className = "pzbf-header__topnav";
-    }
-}
