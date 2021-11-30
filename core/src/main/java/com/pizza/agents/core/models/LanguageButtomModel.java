431e4e76ef9bef67fc60ea8a7674809ff3a30941
@borja.rodriguez <b.rodriguez@codebay-innovation.com>
2021-11-30 14:22:56 +0100

    Language Buttom style

--- core/src/main/java/com/pizza/agents/core/models/LanguageButtomModel.java
+++ core/src/main/java/com/pizza/agents/core/models/LanguageButtomModel.java
@@ -4,13 +4,10 @@ import com.day.cq.wcm.api.Page;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.api.resource.ValueMap;
-import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Optional;
 import org.apache.sling.models.annotations.Via;
 import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
-import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,7 +34,7 @@ public class LanguageButtomModel{
     @ScriptVariable(name="currentPage")
     Page page;
 
-    @Inject @Via("resource")
+    @Inject @Optional @Via("resource")
     private Page navigationRoot;
 
     private Iterator<Page> childIterator;
@@ -50,29 +47,18 @@ public class LanguageButtomModel{
     protected void init() {
         LOG.info("==================  LanguageButtomModel ========================");
         try{
-
-            LOG.info("Page: " + page.getPath());
-            LOG.info("navigationRoot: " + navigationRoot.getPath());
             if(navigationRoot != null) {
                 childIterator = navigationRoot.listChildren();
                 while(childIterator.hasNext()){
                     language = childIterator.next().getName();
-                    LOG.info("language: " + language);
-                    LOG.info("Page Path: " + page.getPath());
                     if(!page.getPath().contains("/"+language+"/")){
                         path = page.getPath().replace("/"+page.getLanguage().toString()+"/","/"+language+"/");
-                        LOG.info("path: " + path);
                         Resource resource = resourceResolver.getResource(path);
-                        LOG.info("resource: " + resource);
                         if(resource != null){
                             paths.add(resource.adaptTo(Page.class));
-                            LOG.info("Paths: " + paths);
                         }
                     }
 
-                }
-                for(int i = 0; i < paths.size(); i++){
-                    LOG.info("Paths: " + paths.get(i).getTitle());
                 }
             }
         }catch (Exception ex){
@@ -81,11 +67,9 @@ public class LanguageButtomModel{
 
     }
 
-
     public List<Page> getPaths(){
         return paths;
     }
-
 
 
 }
